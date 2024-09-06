3a37d565acedd4aeb4f4195f5ac204d42af8a193
Ly, Kim Kim <kim.ly@applydigital.co>
2024-09-06 15:44:49 -0400

    chapter3

--- Chapter 3 - Form Development/Exercise 2 - Error Handling/scripts/script.js
+++ Chapter 3 - Form Development/Exercise 2 - Error Handling/scripts/script.js
@@ -26,33 +26,104 @@ const formValidation = [
 const validateForm = (formData) => {
     const errors = formValidation.filter(formControl => {
         const value = formData[formControl.id]?.value
-        if(formControl.required) {
+        if (formControl.required) {
             return !value;
         }
-        if(formControl.regex  && !!value) {
+        if (formControl?.regex && !!value) {
             const regex = new RegExp(formControl.regex);
-            return !regex.test(value)
+            return !regex.test(value);
+        } else {
+            return false;
         }
+
     });
 
     return errors;
+}
+
+const handleFormErrorsList = (errors) => {
+    const text = errors.length > 1
+        ? `There are ${errors.length} errors in the form submission`
+        : `There is ${errors.length} error in the form submission`
+    const title = `<h3>${text}</h3>`
+    const alertHTML = errors.map(error => `<a href="#${error.id}">${error.errorMessage}</>`).join('');
+    document.getElementById('formErrorsList').innerHTML = title + alertHTML;
+}
+
+const handleFormInputs = (formInputs, errors, reset = false) => {
+    for (let i = 0; i < formInputs.length; i++) {
+        if (formInputs[i].nodeName === "INPUT") {
+            const formInput = formInputs[i];
+            const inputId = formInput.id;
+            const inputGroup = formInput.name;
+
+            const parentNode = document.getElementById(inputId)?.parentNode;
+            const legend = parentNode?.querySelector('legend');
+            const label = parentNode?.querySelector('label');
+            const isFieldset = !!legend;
+
+            const ariaDescribedBy = document.getElementById(formInput.getAttribute('aria-describedby'));
+
+
+            const inputError = errors.find(error => error.id === inputId || error.id === inputGroup); 
+
+            if(inputError) {
+                parentNode.className = 'error'
+                ariaDescribedBy.innerText = inputError.errorMessage;
+                isFieldset 
+                    ? legend.querySelector('strong').innerText = 'Error:'
+                    : label.querySelector('strong').innerText = 'Error:'
+                
+
+            } else {
+                parentNode.className = 'success'
+
+                if(ariaDescribedBy) {
+                    ariaDescribedBy.innerText = '';
+                }
+
+                isFieldset 
+                    ? legend.querySelector('strong').innerText = 'Ok:'
+                    : label.querySelector('strong').innerText = 'Ok:'
+            }
+
+            if(reset) {
+                parentNode.className = ''
+
+                if(ariaDescribedBy) {
+                    ariaDescribedBy.innerText = '';
+                }
+
+                isFieldset 
+                    ? legend.querySelector('strong').innerText = ''
+                    : label.querySelector('strong').innerText = ''
+            }
+        }
+    }
+}
+
+const resetForm = (formInputs) => {
+    document.getElementById('formErrorsList').innerHTML = '';
+    handleFormInputs(formInputs, [], true);
 }
 
 const handleFormSubmit = (event) => {
     event.preventDefault();
     event.stopPropagation();
 
-    const formData = event.target.elements;
+    const form = event.target;
+    const formData = form.elements;
     const errors = validateForm(formData);
 
-    if(errors.length) {
-         // HANDLE DISPLAYING FORM ERRORS ON THE UI
-        console.log('ERRORS:', errors)
-    }else {
-         // HANDLE FORM SUCCESS
-        console.log('SUCCESS');
+    if (errors.length) {
+        handleFormErrorsList(errors);
+        handleFormInputs(formData, errors);
+        return;
+    } else {
+        alert('Form Successfully Submitted');
+        resetForm(formData);
+        form.reset();
     }
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
